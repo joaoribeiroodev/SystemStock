d31330438e4ebe7b1802d79002aadd34417d74db
JOÃO PEDRO LIMA RIBEIRO <232.004057@universidadesenaicimatec.edu.br>
2026-04-11 11:50:40 -0300

    criação das variaveis de ambiente e banco docker add

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -13,23 +13,25 @@ import java.sql.SQLException;
  * @author 232.004057
  */
 public class ConnectionFactory {
-    
-    //dados do banco
+
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASS");
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL =
-            "jdbc:mysql://db:3306/estoque_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "cimatec";
     
     //métodos
     public static Connection getConnection() {
         Connection con = null;
         
         try {
+            if(URL == null || USER == null || PASSWORD == null) {
+                System.out.println("variavel de ambiente");
+                return null;
+            }
             Class.forName(DRIVER);
             con = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Bando de Dados Conectado");
-        } catch (Exception e) {
+        }catch (Exception e) {
             System.out.println("Banco de Dados Não Conectado");
             e.printStackTrace();
         }
